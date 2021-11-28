6df5fd10da524b395ea912bfb512343750a49f58
MCChang macbook <meng.chuan.chang@gmail.com>
2021-11-28 13:13:52 -0500

    revise view controllers
    correct register and login view textfield

--- howareyou/howareyou/AppDelegate.swift
+++ howareyou/howareyou/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "FgYqC6mKFtNuKzTL2tN0mLwu7ktM1qgjGSwO4UBu"
+                    $0.clientKey = "pZRHIfNPs1geTEFXdXw22JawX7fGRQ1W8B5cU3K1"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
