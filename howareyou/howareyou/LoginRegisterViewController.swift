75a71a2535a1e0155e51799b2d41e0b2d04d1400
MCChang macbook <meng.chuan.chang@gmail.com>
2021-12-07 21:13:09 -0500

    integrate first page func
    direct user to either login page or register page

---
+++ howareyou/howareyou/LoginRegisterViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginRegisterViewController.swift
+//  howareyou
+//
+//  Created by Meng-Chuan Chang on 11/28/21.
+//
+
+import UIKit
+
+class LoginRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func toLoginPage(_ sender: Any) {
+        self.performSegue(withIdentifier: "loginPageSegue", sender: nil)
+    }
+    
+    @IBAction func toRegisterPage(_ sender: Any) {
+        self.performSegue(withIdentifier: "registerPageSegue", sender: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
