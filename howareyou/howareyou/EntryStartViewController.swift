e2ffce2b3e894f946c9487c7f26b606225a47237
MCChang macbook <meng.chuan.chang@gmail.com>
2021-12-07 22:50:10 -0500

    add login func
    user can login successfully
    
    user can logout via LOGOUT button
    
    user can stay persistent
    
    fix issue: view in full screen
    
    fix issue: segue setting

---
+++ howareyou/howareyou/EntryStartViewController.swift
@@ -0,0 +1,40 @@
+//
+//  EntryStartViewController.swift
+//  howareyou
+//
+//  Created by Meng-Chuan Chang on 12/7/21.
+//
+
+import UIKit
+import Parse
+
+class EntryStartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginRegisterViewController = main.instantiateViewController(withIdentifier: "LoginRegisterViewController")
+
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+
+        delegate.window?.rootViewController = loginRegisterViewController
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
